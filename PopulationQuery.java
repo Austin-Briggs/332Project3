19974b820134f99b43c90441b47791e81761a22f
Austin-Briggs <austin.c.briggs@gmail.com>
2014-02-27 13:16:44 -0800

    Output/Input printing corrected

--- PopulationQuery.java
+++ PopulationQuery.java
@@ -11,47 +11,47 @@ public class PopulationQuery {
 	public static final int POPULATION_INDEX = 4; // zero-based indices
 	public static final int LATITUDE_INDEX   = 5;
 	public static final int LONGITUDE_INDEX  = 6;
-	
+
 	// parse the input file into a large array held in a CensusData object
 	public static CensusData parse(String filename) {
 		CensusData result = new CensusData();
-		
-        try {
-            BufferedReader fileIn = new BufferedReader(new FileReader(filename));
-            
-            // Skip the first line of the file
-            // After that each line has 7 comma-separated numbers (see constants above)
-            // We want to skip the first 4, the 5th is the population (an int)
-            // and the 6th and 7th are latitude and longitude (floats)
-            // If the population is 0, then the line has latitude and longitude of +.,-.
-            // which cannot be parsed as floats, so that's a special case
-            //   (we could fix this, but noisy data is a fact of life, more fun
-            //    to process the real data as provided by the government)
-            
-            String oneLine = fileIn.readLine(); // skip the first line
 
-            // read each subsequent line and add relevant data to a big array
-            while ((oneLine = fileIn.readLine()) != null) {
-                String[] tokens = oneLine.split(",");
-                if(tokens.length != TOKENS_PER_LINE)
-                	throw new NumberFormatException();
-                int population = Integer.parseInt(tokens[POPULATION_INDEX]);
-                if(population != 0)
-                	result.add(population,
-                			   Float.parseFloat(tokens[LATITUDE_INDEX]),
-                		       Float.parseFloat(tokens[LONGITUDE_INDEX]));
-            }
+		try {
+			BufferedReader fileIn = new BufferedReader(new FileReader(filename));
+			
+			// Skip the first line of the file
+			// After that each line has 7 comma-separated numbers (see constants above)
+			// We want to skip the first 4, the 5th is the population (an int)
+			// and the 6th and 7th are latitude and longitude (floats)
+			// If the population is 0, then the line has latitude and longitude of +.,-.
+			// which cannot be parsed as floats, so that's a special case
+			//   (we could fix this, but noisy data is a fact of life, more fun
+			//    to process the real data as provided by the government)
 
-            fileIn.close();
-        } catch(IOException ioe) {
-            System.err.println("Error opening/reading/writing input or output file.");
-            System.exit(1);
-        } catch(NumberFormatException nfe) {
-            System.err.println(nfe.toString());
-            System.err.println("Error in file format");
-            System.exit(1);
-        }
-        return result;
+			String oneLine = fileIn.readLine(); // skip the first line
+
+			// read each subsequent line and add relevant data to a big array
+			while ((oneLine = fileIn.readLine()) != null) {
+				String[] tokens = oneLine.split(",");
+				if(tokens.length != TOKENS_PER_LINE)
+					throw new NumberFormatException();
+				int population = Integer.parseInt(tokens[POPULATION_INDEX]);
+				if(population != 0)
+					result.add(population,
+							Float.parseFloat(tokens[LATITUDE_INDEX]),
+							Float.parseFloat(tokens[LONGITUDE_INDEX]));
+			}
+
+			fileIn.close();
+		} catch(IOException ioe) {
+			System.err.println("Error opening/reading/writing input or output file.");
+			System.exit(1);
+		} catch(NumberFormatException nfe) {
+			System.err.println(nfe.toString());
+			System.err.println("Error in file format");
+			System.exit(1);
+		}
+		return result;
 	}
 
 	// argument 1: file name for input data: pass this to parse
@@ -62,10 +62,10 @@ public class PopulationQuery {
 		// FOR YOU
 		if(args.length != 4) {
 			System.err.println("Usage: [filename] [num x-dimension buckets]" +
-							   " [num y-dimension buckets] [-v#]");
+					" [num y-dimension buckets] [-v#]");
 			System.exit(1);
 		}
-		
+
 		String filename = args[0];
 		int x = Integer.parseInt(args[1]);
 		int y = Integer.parseInt(args[2]);
@@ -73,7 +73,7 @@ public class PopulationQuery {
 		if (version.equals("-v1")) { 		//version 1, simple and sequential
 			CensusData cData = parse(filename);
 			CensusGroup[] data = cData.data;
-			
+
 			//Get the minimum and maximum latitudes and longitudes
 			float minLon = data[0].longitude;
 			float minLat = data[0].latitude;
@@ -86,151 +86,148 @@ public class PopulationQuery {
 				if (data[i].longitude < minLon) minLon = data[i].longitude;
 				if (data[i].latitude > maxLat) maxLat = data[i].latitude;
 				if (data[i].longitude > maxLon) maxLon = data[i].longitude;
-				
+
 				//Also add to the total US population
 				totalUSPop += data[i].population;
 			}
-			
+
 			Scanner console = new Scanner(System.in);
-			System.out.print("Request a query? (y/n) ");
-			while (console.hasNext() && console.nextLine().equalsIgnoreCase("y")) {
-				//Get the line for the query rectangle numbers 
-				System.out.print("Enter the row and column data for your subrectangle request [west south east north]: ");
-				String[] usRectLine = console.nextLine().split(" ");
-				if (usRectLine.length != 4) {
-					System.err.println("Incorrect number of arguments.");
-				} else {
-					//Process the line to get the query rectangle numbers
-					int west, east, south, north = 0;
-					try {
-						west = Integer.parseInt(usRectLine[0]);
-						south = Integer.parseInt(usRectLine[1]);
-						east = Integer.parseInt(usRectLine[2]);
-						north = Integer.parseInt(usRectLine[3]);
-					} catch (NumberFormatException e) {
-						System.out.println("ERROR: You must input valid numbers.");
-						System.out.print("Request another query? (y/n) ");
-						continue;
-					}
-					
-					//Validate the query inputs
-					try {
-						if (west < 1 || west > x) 		throw new IllegalArgumentException("ERROR: west cannot be less than 1 or greater than "+x+".");
-						if (south < 1 || south > y)		throw new IllegalArgumentException("ERROR: south cannot be less than 1 or greater than "+y+".");
-						if (east < west || east > x) 	throw new IllegalArgumentException("ERROR: east cannot be less than west or greater than "+x+".");
-						if (north < south || north > y) throw new IllegalArgumentException("ERROR: north cannot be less than south or greater than "+y+".");
-					} catch (IllegalArgumentException e) {
-						System.out.println(e.getMessage());
-						System.out.print("Request another query? (y/n) ");
-						continue;
-					}
-				
-					//Create the query Rectangle
-					float dLong = (maxLon - minLon) / x;
-					float dLat 	= (maxLat - minLat) / y;
-					Rectangle qRect = new Rectangle(minLon + dLong * (west - 1),
-													minLon + dLong * east,
-													minLat + dLat * north,
-													minLat + dLat * (south - 1));
-					
-					//Find the total population within the query rectangle and the % of totalUSPop it is
-					int queryPop = 0;
-					for (int i = 0; i < cData.data_size; i++) {
-						CensusGroup cg = cData.data[i];
-						float lat = cg.latitude;
-						float lon = cg.longitude;
-						
-						//If the current CensusGroup is bounded by the query rectangle, 
-						//add its population to queryPop
-						if (lat >= qRect.bottom && lat <= qRect.top && lon >= qRect.left && lon <= qRect.right) {
-							queryPop += cg.population;
-						}
-					}
-					float percentTotalPop = ((float) queryPop * 100) / totalUSPop;
-					
-					//Print the results
-					System.out.println("QUERY RESULTS:");
-					System.out.println("Query population = "+queryPop);
-					System.out.print("Percent of US population = ");
-					System.out.printf("%.2f", percentTotalPop);
-					System.out.println("%");
+			//Get the line for the query rectangle numbers 
+			System.out.println("Please give west, south, east, north coordinates of your query rectangle:");
+			String[] usRectLine = console.nextLine().split(" ");
+			while (usRectLine.length == 4) {
+				//Process the line to get the query rectangle numbers
+				int west, east, south, north = 0;
+				try {
+					west = Integer.parseInt(usRectLine[0]);
+					south = Integer.parseInt(usRectLine[1]);
+					east = Integer.parseInt(usRectLine[2]);
+					north = Integer.parseInt(usRectLine[3]);
+				} catch (NumberFormatException e) {
+					System.out.println("ERROR: You must input valid numbers.");
+					System.out.println("Please give west, south, east, north coordinates of your query rectangle:");
+					usRectLine = console.nextLine().split(" ");
+					continue;
 				}
-				
-				System.out.print("Request another query? (y/n) ");
+
+				//Validate the query inputs
+				try {
+					if (west < 1 || west > x) 		throw new IllegalArgumentException("ERROR: west cannot be less than 1 or greater than "+x+".");
+					if (south < 1 || south > y)		throw new IllegalArgumentException("ERROR: south cannot be less than 1 or greater than "+y+".");
+					if (east < west || east > x) 	throw new IllegalArgumentException("ERROR: east cannot be less than west or greater than "+x+".");
+					if (north < south || north > y) throw new IllegalArgumentException("ERROR: north cannot be less than south or greater than "+y+".");
+				} catch (IllegalArgumentException e) {
+					System.out.println(e.getMessage());
+					System.out.println("Please give west, south, east, north coordinates of your query rectangle:");
+					usRectLine = console.nextLine().split(" ");
+					continue;
+				}
+
+				//Create the query Rectangle
+				float dLong = (maxLon - minLon) / x;
+				float dLat 	= (maxLat - minLat) / y;
+				Rectangle qRect = new Rectangle(minLon + dLong * (west - 1),
+						minLon + dLong * east,
+						minLat + dLat * north,
+						minLat + dLat * (south - 1));
+
+				//Find the total population within the query rectangle and the % of totalUSPop it is
+				int queryPop = 0;
+				for (int i = 0; i < cData.data_size; i++) {
+					CensusGroup cg = cData.data[i];
+					float lat = cg.latitude;
+					float lon = cg.longitude;
+
+					//If the current CensusGroup is bounded by the query rectangle, 
+					//add its population to queryPop
+					if (lat >= qRect.bottom && lat <= qRect.top && lon >= qRect.left && lon <= qRect.right) {
+						queryPop += cg.population;
+					}
+				}
+				float percentTotalPop = ((float) queryPop * 100) / totalUSPop;
+
+				//Print the results
+				System.out.println("population of rectangle: "+queryPop);
+				System.out.print("percent of total population: ");
+				System.out.printf("%.2f", percentTotalPop);
+				System.out.println();
+
+				//Prompt again
+				System.out.println("Please give west, south, east, north coordinates of your query rectangle:");
+				usRectLine = console.nextLine().split(" ");				
 			}
 			console.close();
-			
+
 		} else if (version.equals("-v2")) { //version 2, simple and parallel
 			CensusData cData = parse(filename);
 			CensusGroup[] data = cData.data;
-			
+
 			//Makes the United States Rectangle
 			ForkJoinPool fjPool = new ForkJoinPool();
 			ParallelSquare ps = new ParallelSquare(data,0,cData.data_size);
 			Rectangle usRectangle = fjPool.invoke(ps);
-		
+
 			Scanner console = new Scanner(System.in);
-			System.out.print("Request a query? (y/n) ");
-			while (console.hasNext() && console.nextLine().equalsIgnoreCase("y")) {
-				//Get the line for the query rectangle numbers 
-				System.out.print("Enter the row and column data for your subrectangle request [west south east north]: ");
-				String[] usRectLine = console.nextLine().split(" ");
-				if (usRectLine.length != 4) {
-					System.err.println("Incorrect number of arguments.");
-				} else {
-					//Process the line to get the query rectangle numbers
-					int west, east, south, north = 0;
-					try {
-						west = Integer.parseInt(usRectLine[0]);
-						south = Integer.parseInt(usRectLine[1]);
-						east = Integer.parseInt(usRectLine[2]);
-						north = Integer.parseInt(usRectLine[3]);
-					} catch (NumberFormatException e) {
-						System.out.println("ERROR: You must input valid numbers.");
-						System.out.print("Request another query? (y/n) ");
-						continue;
-					}
-					
-					//Validate the query inputs
-					try {
-						if (west < 1 || west > x) 		throw new IllegalArgumentException("ERROR: west cannot be less than 1 or greater than "+x+".");
-						if (south < 1 || south > y)		throw new IllegalArgumentException("ERROR: south cannot be less than 1 or greater than "+y+".");
-						if (east < west || east > x) 	throw new IllegalArgumentException("ERROR: east cannot be less than west or greater than "+x+".");
-						if (north < south || north > y) throw new IllegalArgumentException("ERROR: north cannot be less than south or greater than "+y+".");
-					} catch (IllegalArgumentException e) {
-						System.out.println(e.getMessage());
-						System.out.print("Request another query? (y/n) ");
-						continue;
-					}
-					
-					float dLong = (usRectangle.right - usRectangle.left) / x;
-					float dLat 	= (usRectangle.top - usRectangle.bottom) / y;
-					Rectangle qRect = new Rectangle(usRectangle.left + dLong * (west - 1),
-													usRectangle.left + dLong * east,
-													usRectangle.bottom + dLat * north,
-													usRectangle.bottom + dLat * (south - 1));
-					SimpleQuery sq = new SimpleQuery(data, 0, cData.data_size,qRect);
-					Pair<Integer,Integer> qp = fjPool.invoke(sq);
-					Integer queryPop = qp.getElementA();
-					Integer totalUSPop = qp.getElementB();
-					float percentTotalPop = ((float) queryPop * 100) / totalUSPop;
-					
-					//Print the results
-					System.out.println("QUERY RESULTS:");
-					System.out.println("Query population = "+queryPop);
-					System.out.print("Percent of US population = ");
-					System.out.printf("%.2f", percentTotalPop);
-					System.out.println("%");
+			//Get the line for the query rectangle numbers 
+			System.out.println("Please give west, south, east, north coordinates of your query rectangle:");
+			String[] usRectLine = console.nextLine().split(" ");
+			while (usRectLine.length == 4) {
+				//Process the line to get the query rectangle numbers
+				int west, east, south, north = 0;
+				try {
+					west = Integer.parseInt(usRectLine[0]);
+					south = Integer.parseInt(usRectLine[1]);
+					east = Integer.parseInt(usRectLine[2]);
+					north = Integer.parseInt(usRectLine[3]);
+				} catch (NumberFormatException e) {
+					System.out.println("ERROR: You must input valid numbers.");
+					System.out.println("Please give west, south, east, north coordinates of your query rectangle:");
+					usRectLine = console.nextLine().split(" ");
+					continue;
 				}
-				System.out.print("Request another query? (y/n) ");
+
+				//Validate the query inputs
+				try {
+					if (west < 1 || west > x) 		throw new IllegalArgumentException("ERROR: west cannot be less than 1 or greater than "+x+".");
+					if (south < 1 || south > y)		throw new IllegalArgumentException("ERROR: south cannot be less than 1 or greater than "+y+".");
+					if (east < west || east > x) 	throw new IllegalArgumentException("ERROR: east cannot be less than west or greater than "+x+".");
+					if (north < south || north > y) throw new IllegalArgumentException("ERROR: north cannot be less than south or greater than "+y+".");
+				} catch (IllegalArgumentException e) {
+					System.out.println(e.getMessage());
+					System.out.println("Please give west, south, east, north coordinates of your query rectangle:");
+					usRectLine = console.nextLine().split(" ");
+					continue;
+				}
+
+				float dLong = (usRectangle.right - usRectangle.left) / x;
+				float dLat 	= (usRectangle.top - usRectangle.bottom) / y;
+				Rectangle qRect = new Rectangle(usRectangle.left + dLong * (west - 1),
+						usRectangle.left + dLong * east,
+						usRectangle.bottom + dLat * north,
+						usRectangle.bottom + dLat * (south - 1));
+				SimpleQuery sq = new SimpleQuery(data, 0, cData.data_size,qRect);
+				Pair<Integer,Integer> qp = fjPool.invoke(sq);
+				Integer queryPop = qp.getElementA();
+				Integer totalUSPop = qp.getElementB();
+				float percentTotalPop = ((float) queryPop * 100) / totalUSPop;
+
+				//Print the results
+				System.out.println("population of rectangle: "+queryPop);
+				System.out.print("percent of total population: ");
+				System.out.printf("%.2f", percentTotalPop);
+				System.out.println();
+				
+				//Prompt again
+				System.out.println("Please give west, south, east, north coordinates of your query rectangle:");
+				usRectLine = console.nextLine().split(" ");
 			}
-		console.close();
+			console.close();
 		} else if (version.equals("-v3")) { //version 3, smarter and sequential
-			
+
 		} else if (version.equals("-v4")) { //version 4, smarter and parallel
-			
+
 		} else if (version.equals("-v5")) { //version 5, smarter and lock-based
-			
+
 		} else { //incorrect input
 			System.err.println("Incorrect version format. Must use -v1, -v2, -v3, -v4, or -v5.");
 			System.exit(1);
